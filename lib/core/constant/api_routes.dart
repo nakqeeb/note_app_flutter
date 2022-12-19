eb50bc20da26303c3a0092590af5485f8ce737cb
Yafey <kjbn.me@gmail.com>
2022-12-19 08:41:34 +0300

    update api link

--- lib/core/constant/api_routes.dart
+++ lib/core/constant/api_routes.dart
@@ -1,4 +1,5 @@
 class ApiRoutes {
-  // static const base_url = '192.168.0.115:3000';
-  static const base_url = 'nodejs-note-app.herokuapp.com';
+  // static const base_url = '192.168.1.10:3000';
+  // static const base_url = 'nodejs-note-app.herokuapp.com'; // Not working anymore
+  static const base_url = 'note-app-zlxz.onrender.com';
 }
